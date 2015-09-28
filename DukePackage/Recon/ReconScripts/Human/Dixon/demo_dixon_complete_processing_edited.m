dcb37399c7bc651ce7868f4a99e1dd23a3af8189
jenniferwang691 <jenniferwang691@gmail.com>
2015-09-28 15:44:56 -0400

    segmented left/right lung, code not uploaded

---
+++ DukePackage/Recon/ReconScripts/Human/Dixon/demo_dixon_complete_processing_edited.m
@@ -0,0 +1,25 @@
+
+% Ask user for all pfiles
+disp('Locate Phase Calibration Pfile please...');
+phaseCal_pfile = filepath();
+[pathstr,name,ext] = fileparts(phaseCal_pfile);
+disp('Locate Dixon Pfile please...');
+dixon_pfile = filepath(pathstr);
+disp('Locate BHUTE Pfile please...');
+bhute_pfile = filepath(pathstr);
+
+% Perform phase calibration or use dyn spectrocsopy if no phase cal
+meanRbc2barrier = demo_dixon_phase_calibration(phaseCal_pfile);
+% meanRbc2barrier = demo_dixon_DynamicSpectAsPhaseCal(phaseCal_pfile);
+
+% Reconstruct ventilation/dissolved phase image
+demo_dixon_recon_ventDis(dixon_pfile);
+% demo_dixon_recon_ventDis_old(dixon_pfile)
+
+% Reconstruct BHUTE and make lung mask 
+% demo_dixon_recon_BHUTE(bhute_pfile, dixon_pfile)
+demo_dixon_recon_BHUTE_edited(bhute_pfile, dixon_pfile)
+
+% Perform dixon decomposition
+% demo_dixon_phaseCorrect_splitRes(meanRbc2barrier, bhute_pfile, dixon_pfile)
+demo_dixon_phaseCorrect_splitRes_edited(meanRbc2barrier, bhute_pfile, dixon_pfile)
